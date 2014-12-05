dc483b0cb2c0e9395d18de771bca3b94019acc7c
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2014-12-05 19:00:09 -0200

    Estrutura base

--- src/WebDevBr/Silex/Silex.php
+++ src/WebDevBr/Silex/Silex.php
@@ -3,16 +3,36 @@
 namespace WebDevBr\Silex;
 
 use Silex\Application;
+use WebDevBr\Doctrine\Doctrine;
 
 class Silex
 {
 	private $app;
 
-	public function __construct()
+	public function __construct($isDevMode = false)
 	{
 		$this->app = new Application();
-		$this->app['debug'] = true;
+        $this->app['debug'] = false;
+        if ($isDevMode)
+		    $this->app['debug'] = true;
 	}
+
+    public function setDoctrine(Doctrine $doctrine)
+    {
+        $this->app['doctrine'] = $this->app->share(function() use($doctrine) {
+            return $doctrine->getEntityManager();
+        });
+    }
+
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    public function setController($name, $controller)
+    {
+        $this->app->mount($name, $controller);
+    }
 
 	public function run()
 	{
